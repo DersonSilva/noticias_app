10d9b7f989b97329a96d0a8b74f94ead2329f83e
Derson Silva <anderson_kiedis@hotmail.com>
2025-10-03 21:19:27 -0300

    Update package-lock.json for Expo compatibility

--- app.config.js
+++ app.config.js
@@ -1,9 +1,37 @@
+// import "dotenv/config";
+
+// export default ({ config }) => ({
+//   ...config,
+//   extra: {
+//     GNEWS_API_KEY: process.env.GNEWS_API_KEY,
+//     GNEWS_BASE_URL: process.env.GNEWS_BASE_URL,
+//   },
+// });
+
+// import "dotenv/config";
+
+// export default ({ config }) => ({
+//   ...config,
+//   extra: {
+//     ...config.extra, // mantém as chaves existentes do app.json
+//     GNEWS_API_KEY: process.env.GNEWS_API_KEY,
+//     GNEWS_BASE_URL: process.env.GNEWS_BASE_URL,
+//     eas: {
+//       projectId: "2d511818-2162-4e8c-8fdd-bfefeb4ca103",
+//     },
+//   },
+// });
+
 import "dotenv/config";
 
 export default ({ config }) => ({
   ...config,
   extra: {
+    ...config.extra, // mantém as chaves existentes
     GNEWS_API_KEY: process.env.GNEWS_API_KEY,
     GNEWS_BASE_URL: process.env.GNEWS_BASE_URL,
+  },
+  eas: {
+    projectId: "2d511818-2162-4e8c-8fdd-bfefeb4ca103",
   },
 });
